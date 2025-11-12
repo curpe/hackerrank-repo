2c291c860f66e5dfb507f172c1fb27a55e42da42
Abhi A <abhi.abduh@gmail.com>
2025-11-12 21:34:20 +0300

    revisions

--- check-odd-even-with-else-if.py
+++ check-odd-even-with-else-if.py
@@ -1,8 +1,8 @@
 # Given an integer, , perform the following conditional actions:
-# If  is odd, print Weird
-# If  is even and in the inclusive range of  to , print Not Weird
-# If  is even and in the inclusive range of  to , print Weird
-# If  is even and greater than , print Not Weird
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than , print Not Weird
 # Input Format
 # A single line containing a positive integer, .
 # Constraints
@@ -14,10 +14,13 @@ if __name__ == '__main__':
     n = int(input().strip())
 
 #using modulo operator
-
-#check odd number
+#remember always add +1 to each end number because python starts counting from 0
 if n % 2 != 0:
     print("Weird")
- #check even number
-elif n % 2 == 0:
+elif n in range(2, 6): #asked range 2 to 5 but we put 2, 6
     print("Not Weird")
+elif n in range(6, 21): #asked range 6 to 20 but we put 2, 6
+    print("Weird")
+elif n > 20:
+    print("Not Weird")
+            
