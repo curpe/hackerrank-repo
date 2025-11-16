32a53067213c5a3d60fd4e4d9b7e7ab8eda2368e
Abhi A <abhi.abduh@gmail.com>
2025-11-16 13:33:07 +0300

    simple loops task

---
+++ loops-task.py
@@ -0,0 +1,20 @@
+
+# Task
+# The provided code stub reads an integer, , from STDIN. For all non-negative integers , print .
+# Example
+
+# The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i * i)
+    
+# Expected output
+0
+1
+4
+9
+16
